e87b6e23ccfb058ccc388cc03551bac1667c8a8d
TreFrost1 <ST10079944@vcconnect.edu.za>
2024-11-20 22:46:57 +0200

    updated settings button

--- app/src/main/java/za/edu/vcconnect/xbcad7319/schoolsync/Settings.kt
+++ app/src/main/java/za/edu/vcconnect/xbcad7319/schoolsync/Settings.kt
@@ -3,30 +3,39 @@ package za.edu.vcconnect.xbcad7319.schoolsync
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class Settings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
 
+
+        val backButton = findViewById<ImageButton>(R.id.bckbutton)
         val editProfileButton = findViewById<Button>(R.id.editProfileButton)
         val messagesButton = findViewById<Button>(R.id.messagesButton)
         val changeLanguageButton = findViewById<Button>(R.id.changeLanguageButton)
         val logoutButton = findViewById<Button>(R.id.logoutButton)
         val Helpbutton = findViewById<Button>(R.id.btn_help_support)
 
+
+
+
         // Handle Edit Profile button click
         editProfileButton.setOnClickListener {
             // Navigate to Edit Profile screen
             val intent = Intent(this, UserProfile::class.java)
             startActivity(intent)
+        }
+
+        backButton.setOnClickListener {
+            backButton.setOnClickListener {
+                val intent = Intent(this, DashboardActivity::class.java)
+                startActivity(intent)
+            }
         }
 
         // Handle Messages button click
@@ -40,6 +49,9 @@ class Settings : AppCompatActivity() {
         changeLanguageButton.setOnClickListener {
             // Show a Toast or open a language selection dialog
             Toast.makeText(this, "Change Language clicked", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, MessagesActivity::class.java)
+            startActivity(intent)
         }
 
         // Handle Logout button click
@@ -55,6 +67,9 @@ class Settings : AppCompatActivity() {
         Helpbutton.setOnClickListener {
             // Show a Toast or open a language selection dialog
             Toast.makeText(this, "Help Button", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, HelpandSupport::class.java)
+            startActivity(intent)
         }
     }
 }
